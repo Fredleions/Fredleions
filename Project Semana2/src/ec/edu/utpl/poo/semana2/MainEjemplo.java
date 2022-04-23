91daedd1e676ed132927e111842618100d7e2eef
Fredleions <faleon13@utpl.edu.ec>
2022-04-23 12:08:27 -0500

    Practica 1 Semana2

---
+++ Project Semana2/src/ec/edu/utpl/poo/semana2/MainEjemplo.java
@@ -0,0 +1,10 @@
+package ec.edu.utpl.poo.semana2;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+public class MainEjemplo {
+    public static void main(String[] args){
+    System.out.println(LocalDate.now());
+    System.out.println(LocalDateTime.now());
+    System.out.println(System.currentTimeMillis());
+    }
+}
